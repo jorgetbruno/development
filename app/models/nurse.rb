81be0eda6069c3c9c9ff562979d437e10da8eb6d
Gilmar Filho <gaxf12@gmail.com>
2016-05-05 21:20:12 -0300

    Redefinição da estrutura do projeto

---
+++ app/models/nurse.rb
@@ -0,0 +1,3 @@
+class Nurse < ActiveRecord::Base
+	belongs_to :user
+end
---
+++ db/migrate/20160505234503_create_nurses.rb
@@ -0,0 +1,10 @@
+class CreateNurses < ActiveRecord::Migration
+  def change
+    create_table :nurses do |t|
+      t.string :name
+      t.belongs_to :user, index: true
+
+      t.timestamps null: false
+    end
+  end
+end
