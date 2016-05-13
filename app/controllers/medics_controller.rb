82ad5c98034948188287d35145cf743131600065
Gilmar Filho <gaxf@hotmail.com>
2016-05-13 11:07:08 +0000

    Adicionado metodo show e create

--- app/controllers/medics_controller.rb
+++ app/controllers/medics_controller.rb
@@ -4,5 +4,22 @@ class MedicsController < ApplicationController
   def new
     @medic = Medic.new
   end
+  def show
+		@medic = Medic.find(params[:id])
+  end
+  def create
+    @medic = Medic.new(medic_params)
 
+	  if @medic.save
+	    redirect_to @medic
+	  else
+		  render 'new'
+	  end
+  end
+
+	private
+
+	def medic_params
+		params.require(:medic).permit(:name, :user_id)
+	end
 end
