e3bd8443f83ae71706291a7107a90d6e70adea75
Gilmar Filho <gaxf@hotmail.com>
2016-05-13 11:36:02 +0000

    Adicionado metodos restantes, excuir não funciona

--- app/controllers/medics_controller.rb
+++ app/controllers/medics_controller.rb
@@ -2,12 +2,15 @@ class MedicsController < ApplicationController
   def index
     @medics = Medic.all
   end
+  
   def new
     @medic = Medic.new
   end
+  
   def show
 		@medic = Medic.find(params[:id])
   end
+  
   def create
     @medic = Medic.new(medic_params)
 
@@ -16,6 +19,27 @@ class MedicsController < ApplicationController
 	  else
 		  render 'new'
 	  end
+  end
+  
+  def edit
+		@medic = Medic.find(params[:id])
+  end
+  
+	def update
+		@medic = Medic.find(params[:id])
+
+		if @medic.update(params[:medic].permit(:name, :user_id))
+			redirect_to @medic
+		else
+			render 'edit'
+		end
+	end
+	
+  def destroy
+		@medic = Medic.find(params[:id])
+		@medic.destroy
+
+		redirect_to medics_path
   end
 
 	private
