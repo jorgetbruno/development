8c878de36ebea69c952a9ef2769f34ddf8df5f01
Gilmar Filho <gaxf@hotmail.com>
2016-05-13 18:49:45 +0000

    Criado metodos new e create para nurse

--- app/controllers/nurses_controller.rb
+++ app/controllers/nurses_controller.rb
@@ -1,4 +1,24 @@
 class NursesController < ApplicationController
   def index
   end
+  
+  def new
+    @nurse = Nurse.new
+  end
+  
+  def create
+    @nurse = Nurse.new(nurse_params)
+
+	  if @nurse.save
+	    redirect_to @nurse
+	  else
+		  render 'new'
+	  end
+  end
+  
+	private
+
+	def nurse_params
+		params.require(:nurse).permit(:name, :user_id)
+	end
 end
