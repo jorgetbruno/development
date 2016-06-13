fca15fc3470ac2612f59a0626410b38ca5372000
eliezerdejj <eliezerdejj@gmail.com>
2016-06-12 21:46:08 -0300

    Add files via upload
    Validação de CRM de médico

--- app/models/medic.rb
+++ app/models/medic.rb
@@ -1,3 +1,7 @@
 class Medic < ActiveRecord::Base
 	belongs_to :user
+	validates :crm, presence: true, length: { minimum: 4, maximum: 10 }
+	validates :emailM, confirmation: true
+	validates :emailM_confirmation, presence: true
 end
+ 	 	 
