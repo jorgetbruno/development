64b73d5867f16c562214979a9221673c2153f648
Jorge_Teixeira_Bruno <jorgetbruno@hotmail.co.uk>
2016-07-15 10:06:04 -0300

    Revert "Retirado declarações redundantes em routes.rb para medics e nurses"
    This reverts commit 745730e652fd13623b2f3cd05e3007501ec50c70.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
-  
-  root 'welcome#index'
+  get 'nurses/index'
 
+  get 'medics/index'
+
+  root 'welcome#index'
+  
   resources :patients
   resources :nurses
   resources :medics
   resources :admins
-
+  
   devise_for :users, controllers: { sessions: "users/sessions" }
 
   # The priority is based upon order of creation: first created -> highest priority.
