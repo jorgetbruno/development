8e179989f5931fa4d3dff114a9b40810de57172d
Jorge_Teixeira_Bruno <jorgetbruno@hotmail.co.uk>
2016-07-14 22:57:32 -0300

    Corrigido model de user, problema com validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,32 +3,32 @@ class User < ActiveRecord::Base
 	has_many :patients
 	has_many :medics
 	has_many :nurses
-	
-	validates :name         , length: {maximum: 100}            , allow_blank: true
-	validates :birthday,	inclusion: { in: 1900..Date.today.year }, 
-										format: { with: /(19|20)\d{2}/i}, 
+
+	validates :name         , length: {maximum: 100}            , allow_blank: false
+	validates :birthday,	inclusion: { in: 1900..Date.today.year },
+										format: { with: /(19|20)\d{2}/i},
     									allow_blank: false
-	
-	validates :lot                                              , allow_blank: false
-	validates :block                                            , allow_blank: false
-	validates :number                                           , allow_blank: false
-	validates :street                                           , allow_blank: true
-	validates :neighborhood                                     , allow_blank: true
-	validates :complement                                       , allow_blank: false
-	
-	validates :city                                             , allow_blank: true
-	validates :state                                            , allow_blank: true
-	validates :country                                          , allow_blank: true
-	validates :cep                                              , allow_blank: true
-	
+
+	validates :lot                        ,length: {minimum: 1}, allow_blank: false
+	validates :block                      ,length: {minimum: 1}, allow_blank: false
+	validates :number                     ,length: {minimum: 1}, allow_blank: false
+	validates :street                     ,length: {maximum: 250}, allow_blank: true
+	validates :neighborhood               ,length: {maximum: 250}, allow_blank: true
+	validates :complement                 ,length: {minimum: 1}, allow_blank: false
+
+	validates :city                       ,length: {maximum: 250}, allow_blank: true
+	validates :state                      ,length: {maximum: 250}, allow_blank: true
+	validates :country                    ,length: {maximum: 250}, allow_blank: true
+	validates :cep                        ,length: {maximum: 250}, allow_blank: true
+
 	validates :phoneOne     , length: {minimum: 10, maximum: 12}, allow_blank: true
 	validates :phoneTwo     , length: {minimum: 10, maximum: 12}, allow_blank: false
 	validates :phoneThree   , length: {minimum: 10, maximum: 12}, allow_blank: false
-	
+
 	validates :cpf          , length: {is: 11}                  , allow_blank: true
 	validates :rg           , length: {maximum: 20}             , allow_blank: true
 	validates :rgExpeditor  , length: {maximum: 11}             , allow_blank: true
-	
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
